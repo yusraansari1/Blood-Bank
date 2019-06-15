3591644d8ce006dbddac00758037b3b767d9df68
Yusra Ansari <47756057+yusraansari1@users.noreply.github.com>
2019-06-15 14:57:10 +0500

    Add files via upload

---
+++ js/authentication.js
@@ -0,0 +1,43 @@
+//Firebase AUTHENTICATION
+
+ 
+function register() {
+    const auth = firebase.auth()
+    const email = document.getElementById("Email").value
+    const password = document.getElementById("PWD").value
+    auth.createUserWithEmailAndPassword(email, password).catch(e =>
+        
+        document.getElementById("errorRegister").innerHTML="***"+ e.message
+    )
+}
+
+
+function logIn() {
+    
+    const auth = firebase.auth();
+    const email = document.getElementById("userEmail").value
+    const pwd = document.getElementById("userPWD").value
+    auth.signInWithEmailAndPassword(email, pwd).catch(e =>
+        document.getElementById("errorLogIn").innerHTML="***"+ e.message
+        
+        
+    )
+}
+
+
+function logOut() {
+    const auth = firebase.auth();
+    auth.signOut().then(function () {
+        // Sign-out successful.
+        window.location = "indexM.html"
+    }).catch(function (error) {
+        // An error happened.
+        console.log(error.message)
+    });
+}
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        window.location="userMain.html"
+        console.log("User Signed In")
+    }
+});
